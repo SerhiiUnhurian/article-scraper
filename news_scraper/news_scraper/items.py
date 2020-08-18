19dccdfed144f1a3bcc0a7abed23e09510fed5af
Serhii Unhurian <unguryan23@gmail.com>
2020-08-18 11:30:06 +0300

    django and scrapy integration with errors

---
+++ news_scraper/news_scraper/items.py
@@ -0,0 +1,7 @@
+from scrapy_djangoitem import DjangoItem
+# from news.models import Article
+
+
+class ArticleItem(DjangoItem):
+    # django_model = Article
+    print('Hiiiiiiiiiiiii!')
---
+++ news_scraper/news_scraper/spiders/news_spider.py
@@ -0,0 +1,34 @@
+import json
+import scrapy
+from news_scraper.items import ArticleItem
+from datetime import datetime, date
+
+# DATE = datetime.date(datetime.now())
+DATE = date(2020, 8, 1)
+
+
+class NewsSpider(scrapy.Spider):
+    name = 'news'
+    page = 1
+    start_urls = [
+        'https://www.grammarly.com/blog/wp-json/grammarly/contenthub/posts/?page=1']
+
+    def parse(self, response):
+        data = json.loads(response.text)
+
+        for article in data:
+            dt = datetime.strptime(article['date'], "%Y-%m-%dT%H:%M:%S")
+            if dt.date() >= DATE:
+                item = ArticleItem()
+                item['title'] = article['title']['rendered']
+                item['content'] = article['content']['rendered']
+                item['date_posted'] = article['date']
+                item['origin_link'] = article['link']
+                yield item
+            else:
+                return
+
+        self.page += 1
+        url = 'https://www.grammarly.com/blog/wp-json/grammarly/contenthub/posts/?page={}'.format(
+            self.page)
+        yield response.follow(url=url, callback=self.parse)
